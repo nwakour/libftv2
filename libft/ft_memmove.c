511f91c645b318693e7bd9830bc5a5c572ee71af
nwakour <nizarlol95@gmail.com>
2022-10-26 19:32:22 +0100

    Adding memove and strcmp

---
+++ libft/ft_memmove.c
@@ -0,0 +1,19 @@
+
+#include "libft.h"
+
+void *ft_memmove(void *dest, const void *src, size_t n)
+{
+    char *d = dest;
+    const char *s = src;
+    if (d < s)
+        while (n--)
+            *d++ = *s++;
+    else
+    {
+        d += n;
+        s += n;
+        while (n--)
+            *--d = *--s;
+    }
+    return (dest);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -9,5 +9,7 @@ void *ft_calloc(size_t size);
 void ft_bzero(void *s, size_t n);
 size_t ft_strlen(const char *s);
 void *ft_memcpy(void *dest, const void *src, size_t n);
+void *ft_memmove(void *dest, const void *src, size_t n);
 void ft_putstr_fd(const char *s, int fd);
+int ft_strcmp(const char *s1, const char *s2);
 #endif
